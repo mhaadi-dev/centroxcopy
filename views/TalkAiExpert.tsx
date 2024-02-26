25fa3b264adfa052f1f27ac08dfb69bb4cff6e9b
Hamzah <hamzahejaz88@gmail.com>
2024-02-26 13:08:10 +0500

    feat: callendly integration

--- views/TalkAiExpert.tsx
+++ views/TalkAiExpert.tsx
@@ -1,13 +1,35 @@
+"use client"
+import { useState } from "react";
+
 export const TalkAiExpert = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const openCalendlyPopup = () => {
+		setIsOpen(true);
+	};
+
+	const closeCalendlyPopup = () => {
+		setIsOpen(false);
+	};
+
 	return (
-		<div id="" className="h-screen">
-			 <h1>Schedule a Meeting</h1>
-      <iframe
-        src="https://calendly.com/hamzahejaz88"
-        style={{ width: '100%', height: '800px', border: 'none' }}
-        scrolling="auto"
-        title="Calendly Scheduling"
-      ></iframe>
+		<div>
+			<button onClick={openCalendlyPopup}>Open Calendly</button>
+			{isOpen && (
+        <div className="calendly-popup" style={{ backgroundColor: '#121212', position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 9999, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div className="calendly-popup-content" style={{ width: '100%', maxWidth: '800px', height: '800px', border: 'none', overflow: 'hidden' }}>
+            <span className="calendly-popup-close" style={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer', color: '#ffffff' }} onClick={closeCalendlyPopup}>
+              &times;
+            </span>
+            <iframe
+              src="https://calendly.com/hamzahejaz88"
+              style={{ width: '100%', height: '100%', border: 'none' }}
+              scrolling="auto"
+              title="Calendly Scheduling"
+            ></iframe>
+          </div>
+        </div>
+      )}
 		</div>
 	);
 };
