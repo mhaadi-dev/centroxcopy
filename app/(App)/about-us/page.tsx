b578e5f28a7ff3e784d106bde64e7fdc3e81ecbe
Muhammad Hadi <mhadi.centrox@gmail.com>
2024-11-08 15:14:24 +0500

    feat: about us page design

---
+++ app/(App)/about-us/page.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import LandingLayout from '../layoutPage'
+import GridPicturesAbout from '@/Components/common/GridPicturesAbout'
+import { HeroSection } from '@/sections/HeroSection'
+import dummyDisplay from "@/assets/dummyDisplay.webp";
+import IndustryBanner from '@/Components/common/IndustryBanner';
+import classNames, { section_wrapper_class, text_h1_main, text_para_main } from '@/helpers/common';
+import aboutbg from "@/assets/aboutbg.webp"
+
+import Image from 'next/image';
+
+const page = () => {
+  return (
+    <LandingLayout>
+      <div className="h-screen   relative mt-24 ">
+     
+        <div className=' rotate-180  '>
+          <Image src={aboutbg} alt='background' className=' w-full h-full object-cover'></Image>
+        </div>
+        <section className={classNames(section_wrapper_class,"flex flex-col gap-4  !mt-0  py-10 lg:py-16")}>
+             <h1 className={classNames(text_h1_main,"text-center")}>Accelerating Your Gen AI Journey</h1>
+        <p className={classNames(text_para_main,"text-center mx-auto")}>Centrox AI is a full-cycle AI development company on a mission to augment startups that are moving the innovation needle in Gen AI. We want to enable you to focus on your core product and achieve your business goals faster while we take over your Gen AI development life cycle. We want to put our AI technical depth, collaborative approach, and commitment to delivering to good use by helping you out.</p>
+        </section>
+        <div className=' '>
+          <Image src={aboutbg} alt='background' className=' w-full h-full object-cover'></Image>
+        </div>
+      </div>
+      <HeroSection btnText='Checkout Our Work' img={dummyDisplay} isGradientText={false} description="Today, Centrox is at the forefront of the Generative AI solutions industry. We collaborate with ambitious startups that are pushing the boundaries of what's possible with LLMs. We provide the technical expertise, strategic guidance, and collaborative approach needed to navigate the complexities of Generative AI development and deployment. Our solutions span various industries, including healthcare, fashion, real estate, and more, empowering startups to achieve their AI goals and gain a competitive edge." heading='What We Do Today: Augmenting Gen AI Startups'/>
+      <GridPicturesAbout headingText='Our Culture & Values' paraText="We're a team of driven individuals who thrive on technical challenges, continuous learning, and open collaboration. We embrace the entrepreneurial spirit and understand the fast-paced nature of startups. Our commitment to transparency, data-driven decision-making, and delivering tangible results sets us apart."
+      btnText='Meet the Team'
+      />
+      <GridPicturesAbout headingText='Our Expertise' paraText="Our team consists of experienced AI engineers and researchers with deep knowledge of LLM architecture, training, optimization, and deployment. We stay at the cutting edge of AI research, actively contributing to the open-source community and ensuring our clients benefit from the latest advancements in Generative AI."
+      btnText='Explore Our Services' reverse
+      />
+      <IndustryBanner btnText="Book 1:1 Session" isBooking heading="Let's Discuss Your Vision." description="Book an exclusive 1:1 call today with our AI expert to discuss and discover what we can do to accelerate your Gen AI development and deployment."/>
+    </LandingLayout>
+  )
+}
+
+export default page
