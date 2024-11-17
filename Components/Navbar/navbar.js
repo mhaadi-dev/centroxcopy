5e0522601bc6d11804204e93aa5c2c6f13a07a6a
Muhammad Hadi <mhadi.centrox@gmail.com>
2024-11-17 19:45:22 +0500

    feat: updates in blog casestudy subnavbar

--- Components/Navbar/navbar.js
+++ Components/Navbar/navbar.js
@@ -356,6 +356,24 @@ export const Navbar = () => {
               ></path>
             </svg>
           </button>}
+          {pathname=="/blogs" && !showMenu &&   <button
+            onClick={() => {router.push("/blogs/search")}}
+            className="lg:hidden text-white p-1 ml-2 rounded-full bg-gray-700/80 "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="25"
+              height="25"
+              viewBox="0 0 30 30"
+            >
+              <path
+                fill="white"
+                d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"
+              ></path>
+            </svg>
+          </button>}
           <div
             className="xl:hidden text-white p-3 hover:rounded-full hover:bg-gray-600 hover:bg-opacity-6 flex flex-col gap-y-1 hover"
             onClick={() => {
