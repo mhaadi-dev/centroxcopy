bb2bb1c5805c15cb47b7cce330ca011f8e359318
Muhammad Hadi <mhadi.centrox@gmail.com>
2024-12-16 11:08:27 +0500

    fix: robottxt fix

--- app/robots.ts
+++ app/robots.ts
@@ -3,12 +3,16 @@ import { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      { userAgent: '*', disallow: '/' },
-      { userAgent: '*', allow: '/*.ts' },
-      { userAgent: '*', allow: '/*.tsx' },
-      { userAgent: '*', allow: '/services/' },
-      { userAgent: '*', allow: '/solutions/' },
-      { userAgent: '*', allow: '/industries/' },
+      {
+        userAgent: '*',
+        disallow: '/',
+        allow: [
+          '/*.tsx',
+          '/services/',
+          '/solutions/',
+          '/industries/',
+        ],
+      },
     ],
     sitemap: 'https://centrox.ai/sitemap.xml',
   };
