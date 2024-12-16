f80633a82ec8ec4338a5ea8ea6c31a9ece7832f2
Muhammad Hadi <mhadi.centrox@gmail.com>
2024-12-16 11:18:05 +0500

    fix: robottxt fix

--- app/robots.ts
+++ app/robots.ts
@@ -8,9 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: '/',
         allow: [
           '/*.tsx',
-          '/services/',
-          '/solutions/',
-          '/industries/',
         ],
       },
     ],
