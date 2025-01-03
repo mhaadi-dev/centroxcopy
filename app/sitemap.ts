c5e1372c63c4a0aa0857d485aa31726c7de5de11
mhadi.centrox <mhadi.centrox@gmail.com>
2025-01-03 16:48:31 +0500

    fix: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -32,7 +32,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   try {
     const fetchLink =  isStaging ? "https://staging.centrox.ai/api/fetchsitemapblogs" : "https://centrox.ai/api/fetchsitemapblogs"
     const localLink = 'http://localhost:3000/api/fetchsitemapblogs'
-    const response = await fetch( fetchLink,{cache:"no-cache"});
+    const response = await fetch( localLink,{cache:"no-cache"});
     const data = await response.json();
     const dynamicLinks_blog_detail = data.map((blog: any) => ({
       url: `https://centrox.ai/blogs/${blog.category?.category_name}/${blog.label.current}`,
