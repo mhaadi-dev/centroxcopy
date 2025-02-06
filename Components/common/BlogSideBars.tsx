b056f76bce395a433b1b1cc52d835f8ef5ee61ef
mhadi.centrox <mhadi.centrox@gmail.com>
2025-02-06 15:42:19 +0500

    feat: solution integration snap and measure and blog sidebar

--- Components/common/BlogSideBars.tsx
+++ Components/common/BlogSideBars.tsx
@@ -7,13 +7,11 @@ const BlogSideBars = () => {
   return (
     <div className='hidden lg:flex flex-col gap-y-6   sticky right-0 left-0 top-[6.5rem] w-[20%] h-full'>
       <div className={classNames(' flex flex-col items-center gap-y-3 text-[0.55rem] lg:text-[1rem] 2xl:text-[1.25rem] font-heading font-semibold leading-[1.6rem] 2xl:leading-[2rem] bg-gradient-to-b from-[#079DFC] to-[#72EFDD] rounded-xl p-6 text-black ')}>
-      <p>We will help you develope what ever you desire in your AI developement. This is a place holder</p> 
+      <p>Do you have an AI idea? Let's Discover the Possibilities Together!
+      From Idea to Innovation; Bring Your AI solution to Life with Us!</p> 
       <CalendlyWidget btnText='Book a Call' btnClassName='!mt-10'></CalendlyWidget>
       </div>
-      <div className={classNames(' flex h-full flex-col items-center gap-y-3  font-heading font-semibold leading-[1.6rem] 2xl:leading-[2rem] bg-gradient-to-b from-[#079DFC] to-[#72EFDD] rounded-xl p-6 text-black ')}>
-      <p>We will help you develope what ever you desire in your AI developement. This is a place holder</p> 
-      {/* <CalendlyWidget btnText='Book a Call' btnClassName='!mt-10'></CalendlyWidget> */}
-      </div>
+   
     </div>
   )
 }
