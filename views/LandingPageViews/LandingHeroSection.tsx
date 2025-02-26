44986e81d87512de97612bc5a6b1aee1d28e3481
mhadi.centrox <mhadi.centrox@gmail.com>
2025-02-26 13:43:39 +0500

    fix: updats

--- views/LandingPageViews/LandingHeroSection.tsx
+++ views/LandingPageViews/LandingHeroSection.tsx
@@ -11,12 +11,15 @@ const LandingHeroSection = () => {
           <Image
             id="hero-bg"
             src={"https://centrox-landing-bucket.s3.eu-north-1.amazonaws.com/home/hero-bg.webp"}
-            className="absolute inset-0  lg:w-full lg:h-full h-full    w-full object-cover  opacity-80"
+            className="absolute inset-0  lg:w-full lg:h-full h-full    w-full object-cover  opacity-100"
             alt="hero-bg"
-            width={1267}
-            height={590}
+            width={1920}
+            height={895}
             priority={true}
           />
+
+          
+         
     
           <div className="relative z-10 flex flex-col gap-3 sm:gap-[1.5rem] items-center text-center px-4 sm:px-0 w-full h-full justify-center">
             <h1 className="text-white text-2xl sm:text-4xl lg:text-5xl 2xl:text-6xl font-heading font-bold">
