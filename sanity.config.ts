8e9148ac58810cfd93e441d7bf7d84f4904c6a60
mhadi.centrox <mhadi.centrox@gmail.com>
2025-03-05 11:50:21 +0500

    feat: preview and edit blogs before publish

--- sanity.config.ts
+++ sanity.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     presentationTool({
 
       previewUrl: {
-      origin: 'http:localhost:3000',
+      origin: 'https://staging.centrox.ai/',
 
         draftMode: {
           enable: "/api/preview",
