f78d205b47302181b3eadadf1b3167f3a01bde75
mhaadi-dev <mhadi.centrox@gmail.com>
2025-03-25 11:10:44 +0500

    fix: robots

--- app/robots.ts
+++ app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
         ...(isStaging
           ? { disallow: '/' } 
           : {
-              allow: ['/blogs', '/*.tsx'], 
+              allow: ['/blogs', '/'], 
               disallow: ['/blogs/search', '/studio'],
             }),
       },
